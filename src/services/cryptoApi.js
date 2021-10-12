592d28e16342d08a377983f82878c7b5e70e9e48
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-10-12 10:05:50 +0300

    react toolkit setup and global crypto stats

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'eb86b347c5msh0c977f7c75d46f2p1a7b2ejsncf42ce95f603',
+};
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins'),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
